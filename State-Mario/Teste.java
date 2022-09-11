66f295b0f514d6cc0009e9ff7d387602189a1f3b
DanielRodrigues87 <67766681+DanielRodrigues87@users.noreply.github.com>
2022-09-11 10:38:24 -0300

    Primeiro commit

---
+++ State-Mario/Teste.java
@@ -0,0 +1,14 @@
+public class Teste {
+    
+    public static void main(String[] args) {
+        
+        Mario mario = new Mario();
+
+        mario.pegarCogumelo();
+        mario.pegarFlor();
+        mario.pegarCogumelo();
+        mario.levarDano();
+        mario.levarDano();
+
+    }
+}
